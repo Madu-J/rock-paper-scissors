3d4b55c690fbd30ad3b83116917c5ae978d50401
Madu-J Omenka <madu.omenka@hotmail.com>
2024-02-28 22:12:55 +0000

    Set up game instruction

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+print("Welcome to Game box!")
+# Let player choose if they want to play or quit
+player = input("Do you want to play Rock, Paper, Scissors Game? ")
+print()
+# Check If user condition is true. If yes then play else quit
+if player.lower() != "yes":
+    quit()
+else:
+    print("Here is the rules for the game ")
+
+choose = ("rock", "paper", "scissors")
+player = None
+computer = random.choice(choose)
+
+while player not in choose:
+    player = input("Make a choice (rock, paper, scissors): ")
+
+print(f"Player: {player}")
+print(f"Computer: {computer}")
+
+if player == computer:
+    print("It's a tie!")
+elif player == "rock" and computer == "scissors":
+    print("You win!")
+elif player == "paper" and computer == "rock":
+    print("You win!")
+elif player == "scissors" and computer == "paper":
+    print("You win!")
+else:
+    print("You lose!")
