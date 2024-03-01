232da0f3241ba058e1ec37fc962dd8f5e19c1359
Madu-J Omenka <madu.omenka@hotmail.com>
2024-03-01 14:30:10 +0000

    Updated game instructions

--- run.py
+++ run.py
@@ -1,15 +1,14 @@
 import random
 
-print("Welcome to Game box!")
-# Let player choose if they want to play or quit
-player = input("Do you want to play Rock, Paper, Scissors Game? ")
+print("Welcome to Game Box!")
+# Player chooses whether to play or quit
+player = input("Do you want to play Rock, Paper, Scissors Game?")
 print()
-# Check If user condition is true. If yes then play else quit
+
 if player.lower() != "yes":
     quit()
 else:
-    print("Here is the rules for the game ")
-
+    print("Here is the rules: The computer will pick a random choice, but in normal contex the rock wins against scissors, paper wins against rock, and scissors wins against paper.")
 choose = ("rock", "paper", "scissors")
 player = None
 computer = random.choice(choose)
@@ -21,12 +20,14 @@ print(f"Player: {player}")
 print(f"Computer: {computer}")
 
 if player == computer:
-    print("It's a tie!")
+    print("It's a tie ! ")
 elif player == "rock" and computer == "scissors":
-    print("You win!")
+    print("You win ! ")
 elif player == "paper" and computer == "rock":
-    print("You win!")
+    print("You win ! ")
 elif player == "scissors" and computer == "paper":
-    print("You win!")
+    print("You win ! ")
 else:
-    print("You lose!")
+    print("You lose ! ")
+
+print("Thanks for playing ! ")
